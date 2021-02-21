96d442f4cec4808569ebf0e8510606705340b96b
Angelo Reis <angelo.reis@kumulus.com.br>
2021-02-21 15:12:59 -0300

    feat: atualização de dependências

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import React from "react"
-import Layout from "../src/components/Layout"
+import React from 'react';
+import Layout from '../src/components/Layout';
 
-import "../styles/styles.css"
+import '../styles/globals.css';
 
-const myApp = ({Component, pageProps}) => {
-    return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    );
+const myApp = ({ Component, pageProps }) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 
 export default myApp;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 const currentYear = () => {
   const DateTemp = new Date();
-  console.log(DateTemp);
   return DateTemp.getFullYear();
 };
 
 const Footer = () => {
   return (
     <div className='flex flex-wrap justify-center content-center bg-gray-600 w-full h-64'>
-      <div className='grid grid-cols-3 gap-4'>
+      <div className='grid grid-cols-3 gap-10 font-extrabold text-white'>
         <div>Feito por @AngeloReis</div>
         <div>Plataforma NextJs</div>
         <div>KFilmes&copy; 2019 - {currentYear()}</div>
