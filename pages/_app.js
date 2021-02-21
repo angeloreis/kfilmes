a382d51b3b9d88f61d76c6fd2b227e76936c5502
Angelo Reis <angelo.reis@kumulus.com.br>
2021-02-20 23:23:23 -0300

    fix: excluindo tudo de bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react"
+import Layout from "../src/components/Layout"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+const myApp = ({Component, pageProps}) => {
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
+};
