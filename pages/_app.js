bc7709b1a2852e89a7d750510e3013d6c84a3e52
Angelo Reis <angelo.reis@kumulus.com.br>
2021-02-21 15:33:57 -0300

    feat: add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Head from 'next/head'
 import Layout from '../src/components/Layout';
 
 import '../styles/globals.css';
 
 const myApp = ({ Component, pageProps }) => {
   return (
+   
     <Layout>
+       <Head>
+        <link rel='shortcut icon' href='images/favicon.ico' />
+       </Head>
       <Component {...pageProps} />
     </Layout>
   );
