573fde326b8dc02e67ff8401adbdb587d790ca17
Angelo Reis <angelo.reis@kumulus.com.br>
2021-06-30 18:55:43 -0300

    fix: removendo coisa inutil

--- utils/google/spreadsheet/index.js
+++
@@ -1,5 +0,0 @@
-import { fromBase64 } from '../../base64';
-export const credentials = {
-  client_email: process.env.SHEET_CLIENT_EMAIL,
-  private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
-};
