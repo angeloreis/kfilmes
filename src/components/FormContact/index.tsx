c1fb270a3a3d294b71bb43da63b2a98ab6e277cc
Angelo Reis <angelo.desenvolvedor@gmail.com>
2021-09-27 22:44:54 -0300

    fix: url whatsapp and style (#7)

--- src/components/FormContact/index.tsx
+++ src/components/FormContact/index.tsx
@@ -1,29 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { FaPaperPlane } from 'react-icons/fa'
+
 import styles from './styles.module.scss'
+import { Console } from 'console'
 export const FormContact = () => {
+    const [username, setUserName] = useState('');
+    const [phonewhats, setPhoneWhats] = useState('');
+    const [textContact, setTextContact] = useState('');
+    const [whatsAppUrl, setWhatsAppUrl] = useState('');
+
+    const router = useRouter();
+
+    function handleSubmit() {        
+        const msgZap = `${textContact}. Meu contato é este: ${phonewhats} - ${username} - Contato direto do site!`;
+        setWhatsAppUrl(`https://wa.me/559188497692?text=${msgZap}`);        
+    }
+
+    useEffect(()=>{
+        router.push(whatsAppUrl);
+    },[whatsAppUrl])
+
     return (
-        <form className={styles.ContainerForm}>
-                <label>
-                Nome Completo
-                </label>
+        <form className={styles.ContainerForm} onSubmit={handleSubmit}>
                 <input
                 type="text"
+                placeholder="Informe seu nome completo"
+                required={true}
+                onChange={(event) => setUserName(event.target.value)}
                 />
-                <label>
-                WhatsApp 
-                </label>
                 <input
                 type="text"
-                placeholder="+55 (99) 9999-9999"                
+                placeholder="WhatsApp: (99) 9.9999-9999"                
+                required={true}
+                onChange={(event) => setPhoneWhats(event.target.value)}
                 />
-                <label>
-                Assunto
-                </label>                
                 <input
-                type=""
+                type="text"
                 placeholder="Digite aqui seu texto"
+                required={true}
+                onChange={(event) => setTextContact(event.target.value)}
                 />
-                <button type="submit">Enviar</button>
+                <button type="submit">Enviar <FaPaperPlane/></button>
         </form>
     );
 }
