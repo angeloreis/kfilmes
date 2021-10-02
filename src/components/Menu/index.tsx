1b0b8ab5bb509c60dca47b97154f046493b1f3df
Angelo Reis <angelo.reis@kumulus.com.br>
2021-10-02 15:21:54 -0300

    fix: menu site
    adjustment to increment responsibility devices

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,26 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import React from 'react'
-import Link from 'next/link'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
-import styles from './styles.module.scss'
+import { FaBars } from 'react-icons/fa'
 
 const Menu = () => {
   return (
-    <nav className={styles.MenuNavigation}>
-      <div>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="#about-us">Quem Somos</a></li>
-            <li><a href="#operate-area">Área que Atuamos</a></li>
-            <li><a href="#our-costumers">Nossos Clientes</a></li>
-            <li><a href="#studio">Estúdio</a></li>
-            <li><a  href="#contact">Contato</a></li>          
-        </ul>
-      </div>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+      <Navbar.Brand className="fw-bolder text-uppercase" href="/" >KFilmes</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+        <Nav className="mr-auto my-1 my-lg-3 h5 text-white">
+          <Nav.Link href="#about-us">Quem Somos</Nav.Link>
+          <Nav.Link href="#operate-area">Área que Atuamos</Nav.Link>
+          <Nav.Link href="#our-costumers">Nossos Clientes</Nav.Link>
+          <Nav.Link href="#studio">Estúdio</Nav.Link>
+          <Nav.Link href="#contact">Contato</Nav.Link>          
+        </Nav>
+      </Navbar.Collapse>
+      </Container>      
+    </Navbar>    
   )
 }
 
