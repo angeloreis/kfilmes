eb1bfcaccc2700faa05aa0f2692b98d1e5b65a20
Angelo Reis <angelo.reis@kumulus.com.br>
2021-10-02 15:21:08 -0300

    fix: component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import styles from './styles.module.scss'
-
 interface HeaderProps {
   children: React.ReactNode
 }
 
 export const Header: React.FC<HeaderProps> = ({ children }) => {
-  return <header className={styles.layoutHeader}>{children}</header>
+  return <header className="container-fluid">{children}</header>;  
 }
