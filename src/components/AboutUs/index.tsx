4314b0b134125f38f6c7edec53c5922c09af5704
Angelo Reis <angelo.reis@kumulus.com.br>
2021-10-02 15:20:53 -0300

    fix: component aubout us

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -1,46 +1,57 @@
 /* eslint-disable @next/next/no-img-element */
+import ContainerSection from 'components/ContainerSection'
 import React from 'react'
-import HeaderTitleSection from '../HeaderSection'
+import { Col, Row, Image, Container } from 'react-bootstrap'
 
-import styles from './styles.module.scss'
-
-export const AboutUs = () => {
+const AboutUs = () => {
   return (
-    <div className={styles.WrapperAboutUsMain}>
-      <div>
-        <p>
-          Quando você adiciona{' '}
-          <strong>QUALIDADE, AGILIDADE E CRIATIVIDADE,</strong> com pessoas
-          apaixonadas pelo que fazem, o resultado surpreende.
-        </p>
-        <p>
-          Nosso diferencial é o <strong>AMOR</strong> ao qual colocamos em
-          nossos diversos serviços. Vamos além de equipamentos modernos e de
-          altíssima resolução, <strong>NOSSO FOCO</strong> é{' '}
-          <strong>EMOCIONAR</strong>, arrepiar a pele e trazer lagrimas de
-          emoção a quem assiste o que fazemos.
-        </p>
-        <p>
-          Seja o nosso Motion graphics <strong>CRIATIVO</strong>, os nossos
-          roteiros que tocam os sentimentos ou nossa edição{' '}
-          <strong>DINÂMICA</strong>.          
-        </p>
-        <p>
-          Em cada take, seja ele na terra ou no ar, sempre buscamos e{' '}
-          <strong>ALCANÇAMOS </strong>
-          algo novo e <strong>ESPETACULAR</strong>. Estamos prontos para somar
-          forças com vocês e atender seus clientes de forma{' '}
-          <strong>INOVADORA</strong>, em todos os formatos disponíveis no{' '}
-          <strong>MERCADO.</strong>
-        </p>
-        <p>
-            Nos somos a <strong>K FILMES!</strong>
-            <p>Somos a sua mais nova opção</p>
-          </p>
-      </div>     
-      <div className={styles.AboutUsRightImg}>
-        <img src='img/about-us-kfilmes-right.png' min-height="70%" alt="K Filmes Digital - Quem Somos"/>
-      </div>
-    </div>
+    <>  
+        <Row className="container bg-dark">
+          <Col xxl xl lg md>
+            <div className="text-white">
+              <p className="h2 text-uppercase fw-bolder">Quem Somos?</p>
+              <p>
+                Quando você adiciona{' '}
+                <strong>QUALIDADE, AGILIDADE E CRIATIVIDADE,</strong> com pessoas
+                apaixonadas pelo que fazem, o resultado surpreende.
+              </p>
+              <p>
+                Nosso diferencial é o <strong>AMOR</strong> ao qual colocamos em
+                nossos diversos serviços. Vamos além de equipamentos modernos e de
+                altíssima resolução, <strong>NOSSO FOCO</strong> é{' '}
+                <strong>EMOCIONAR</strong>, arrepiar a pele e trazer lagrimas de
+                emoção a quem assiste o que fazemos.
+              </p>
+              <p>
+                Seja o nosso Motion graphics <strong>CRIATIVO</strong>, os nossos
+                roteiros que tocam os sentimentos ou nossa edição{' '}
+                <strong>DINÂMICA</strong>.          
+              </p>
+              <p>
+                Em cada take, seja ele na terra ou no ar, sempre buscamos e{' '}
+                <strong>ALCANÇAMOS </strong>
+                algo novo e <strong>ESPETACULAR</strong>. Estamos prontos para somar
+                forças com vocês e atender seus clientes de forma{' '}
+                <strong>INOVADORA</strong>, em todos os formatos disponíveis no{' '}
+                <strong>MERCADO.</strong>
+              </p>
+              <p>
+                Nos somos a <strong>K FILMES!</strong>
+                <p>Somos a sua mais nova opção</p>
+              </p>
+            </div>
+          </Col>
+          <Col xxl xl lg md>
+            <Image
+              src='img/about-us-kfilmes-right.png'
+              alt="K Filmes Digital - Quem Somos"
+              height="450px"
+              className="float-end"
+              />
+          </Col>
+        </Row>      
+    </>    
   )
 }
+
+export default AboutUs
