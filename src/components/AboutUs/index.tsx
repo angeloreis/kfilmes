e1212c6b3151b0d07e3928ee65350356b3038765
Angelo Reis <angelo.desenvolvedor@gmail.com>
2021-12-10 18:13:10 -0300

    fix: remove stack

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -1,55 +1,52 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
-import { Stack, Box, Image, Text } from '@chakra-ui/react'
+import React from "react";
+import { Box, Image, Text } from "@chakra-ui/react";
 
 export function AboutUs() {
   return (
     <>
-    <Stack
-      gap='4'
-      direction='row'
-      align='center'
-    >
-      <Box w='350px' spacing='4' px='8'>
-        <Text as='h1' fontSize='40px' fontWeight='bold'>Quem Somos?</Text>
-        <Text align='justify' >
-          Quando você adiciona{' '}
+      <Box w="350px" spacing="4" px="8">
+        <Text as="h1" fontSize="40px" fontWeight="bold">
+          Quem Somos?
+        </Text>
+        <Text align="justify">
+          Quando você adiciona{" "}
           <strong>QUALIDADE, AGILIDADE E CRIATIVIDADE,</strong> com pessoas
           apaixonadas pelo que fazem, o resultado surpreende.
         </Text>
-        <Text  align='justify' >
+        <Text align="justify">
           Nosso diferencial é o <strong>AMOR</strong> ao qual colocamos em
           nossos diversos serviços. Vamos além de equipamentos modernos e de
-          altíssima resolução, <strong>NOSSO FOCO</strong> é{' '}
+          altíssima resolução, <strong>NOSSO FOCO</strong> é{" "}
           <strong>EMOCIONAR</strong>, arrepiar a pele e trazer lagrimas de
           emoção a quem assiste o que fazemos.
         </Text>
-        <Text  align='justify' >
+        <Text align="justify">
           Seja o nosso Motion graphics <strong>CRIATIVO</strong>, os nossos
-          roteiros que tocam os sentimentos ou nossa edição{' '}
+          roteiros que tocam os sentimentos ou nossa edição{" "}
           <strong>DINÂMICA</strong>.
         </Text>
-        <Text  align='justify' >
-          Em cada take, seja ele na terra ou no ar, sempre buscamos e{' '}
+        <Text align="justify">
+          Em cada take, seja ele na terra ou no ar, sempre buscamos e{" "}
           <strong>ALCANÇAMOS </strong>
           algo novo e <strong>ESPETACULAR</strong>. Estamos prontos para somar
-          forças com vocês e atender seus clientes de forma{' '}
-          <strong>INOVADORA</strong>, em todos os formatos disponíveis no{' '}
+          forças com vocês e atender seus clientes de forma{" "}
+          <strong>INOVADORA</strong>, em todos os formatos disponíveis no{" "}
           <strong>MERCADO.</strong>
         </Text>
-        <br/>
-        <Text align='justify'>
+        <br />
+        <Text align="justify">
           Nos somos a <strong>K FILMES!</strong>
           <Text>Somos a sua mais nova opção</Text>
         </Text>
       </Box>
-      <Box spacing='4' px='8'>
-      <Image
-              src='img/about-us-kfilmes-right.png'
-              alt="K Filmes Digital - Quem Somos"
-              height="660px"/>
-      </Box>      
-    </Stack>
+      <Box spacing="4" px="8">
+        <Image
+          src="img/about-us-kfilmes-right.png"
+          alt="K Filmes Digital - Quem Somos"
+          height="660px"
+        />
+      </Box>
     </>
-  )
+  );
 }
